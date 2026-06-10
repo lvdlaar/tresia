f63da3a7b14cc7bbdcd81967bd88b97795b114f1
Lennard van de Laar <lvdlaar@dualler.nl>
2026-06-10 13:46:04 +0200

    Move inline Swiper config into tresia.js

--- tresia.js
+++ tresia.js
@@ -6,8 +6,46 @@ function removeTrailingChevronFromButtons() {
 	});
 }
 
-if (document.readyState === 'loading') {
-	document.addEventListener('DOMContentLoaded', removeTrailingChevronFromButtons);
-} else {
+function initSwiper() {
+	if (typeof Swiper === 'undefined') {
+		return;
+	}
+
+	if (!document.querySelector('.swiper')) {
+		return;
+	}
+
+	new Swiper('.swiper', {
+		slidesPerView: 1.3,
+		loop: true,
+		autoplay: {
+			delay: 2000,
+		},
+		spaceBetween: 25,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: false,
+		breakpoints: {
+			768: {
+				spaceBetween: 50,
+			},
+			1200: {
+				spaceBetween: 70,
+				slidesPerView: 1.5,
+			},
+		},
+	});
+}
+
+function initPageScripts() {
 	removeTrailingChevronFromButtons();
+	initSwiper();
+}
+
+if (document.readyState === 'loading') {
+	document.addEventListener('DOMContentLoaded', initPageScripts);
+} else {
+	initPageScripts();
 }
